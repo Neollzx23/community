4b661ec6ba1fffd98cfdcb0249c71bda1fd7484c
lzx <547593256@qq.com>
2019-12-16 22:11:59 +0800

    init repo

---
+++ src/main/java/com/lzx/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.lzx.community.controller;
+
+import com.sun.org.glassfish.gmbal.ParameterNames;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
